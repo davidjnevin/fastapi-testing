e75280aafd86dda72e237ca0fa2f7f19d6f2ea8f
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-30 15:01:53 +0200

    Update to read_ping

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
-def test_read_main():
+def test_read_ping():
 
     response = client.get("/ping")
     assert response.status_code == 200
